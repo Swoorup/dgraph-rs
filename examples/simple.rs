8d7091b58ae404e55736cb8a9d3eb890ba0e1393
Ondrej Slinták <ondrowan@gmail.com>
2020-03-29 20:00:10 +0200

    Refactor simple and TLS examples

--- examples/simple.rs
+++ examples/simple.rs
@@ -5,40 +5,42 @@ use dgraph::{make_dgraph, Dgraph};
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct Root {
-    pub me: Vec<Person>,
+struct Root {
+    me: Vec<Person>,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct School {
-    pub name: String,
+struct School {
+    name: String,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct Location {
+struct Location {
     #[serde(rename = "type")]
-    pub kind: String,
-    pub coordinates: Vec<f64>,
+    kind: String,
+    coordinates: Vec<f64>,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct Person {
-    pub name: String,
-    pub age: Option<u8>,
-    pub dob: Option<DateTime<Utc>>,
-    pub married: Option<bool>,
-    pub friend: Option<Vec<Person>>,
-    pub loc: Option<Location>,
-    pub school: Option<Vec<School>>,
+struct Person {
+    name: String,
+    age: Option<u8>,
+    dob: Option<DateTime<Utc>>,
+    married: Option<bool>,
+    friend: Option<Vec<Person>>,
+    loc: Option<Location>,
+    school: Option<Vec<School>>,
 }
 
-fn drop_all(dgraph: &Dgraph) {
-    let op_cleanup = dgraph::Operation {
+fn drop_schema(dgraph: &Dgraph) {
+    let op_drop = dgraph::Operation {
         drop_all: true,
         ..Default::default()
     };
 
-    dgraph.alter(&op_cleanup).expect("drop schema");
+    dgraph.alter(&op_drop).expect("Failed to drop schema.");
+
+    println!("Dropped the schema.");
 }
 
 fn set_schema(dgraph: &Dgraph) {
@@ -54,17 +56,27 @@ fn set_schema(dgraph: &Dgraph) {
         ..Default::default()
     };
 
-    dgraph.alter(&op_schema).expect("set schema");
+    dgraph.alter(&op_schema).expect("Failed to set schema.");
+
+    println!("Altered schema.");
 }
 
-fn create_data(dgraph: &Dgraph) {
+fn main() {
+    let dgraph = make_dgraph!(dgraph::new_dgraph_client("localhost:19080"));
+
+    println!("Connected to dgraph via gRPC at localhost:19080.");
+
+    drop_schema(&dgraph);
+    set_schema(&dgraph);
+
     let mut txn = dgraph.new_txn();
 
-    let dob = Utc.ymd(1980, 1, 1).and_hms(23, 0, 0);
-    // While setting an object if a struct has a Uid then its properties in the graph are updated
-    // else a new node is created.
-    // In the example below new nodes for Alice, Bob and Charlie and school are created (since they
-    // dont have a Uid).
+    // While setting an object if a struct has a Uid then its properties in
+    // the graph are updated. Else a new node is created.
+    //
+    // In the example below new nodes for Alice, Bob and Charlie and school
+    // are created (since they don't have a Uid).
+    let date_of_birth = Utc.ymd(1980, 1, 1).and_hms(23, 0, 0);
     let p = Person {
         name: "Alice".to_string(),
         age: Some(26),
@@ -73,7 +85,7 @@ fn create_data(dgraph: &Dgraph) {
             kind: "Point".to_string(),
             coordinates: vec![1.1f64, 2f64],
         }),
-        dob: Some(dob),
+        dob: Some(date_of_birth),
         friend: Some(vec![
             Person {
                 name: "Bob".to_string(),
@@ -93,30 +105,21 @@ fn create_data(dgraph: &Dgraph) {
 
     // Run mutation
     let mut mutation = dgraph::Mutation::new();
-    mutation.set_set_json(serde_json::to_vec(&p).expect("invalid json"));
-    let assigned = txn.mutate(mutation).expect("failed to create data");
+    mutation.set_set_json(serde_json::to_vec(&p).expect("Failed to serialize JSON."));
+    let assigned = txn.mutate(mutation).expect("Failed to create data.");
 
     // Commit transaction
-    txn.commit().expect("Fail to commit mutation");
+    txn.commit().expect("Failed to commit mutation");
 
-    // Get uid of the outermost object (person named "Alice").
-    // Assigned#getUidsMap() returns a map from blank node names to uids.
-    // For a json mutation, blank node names "blank-0", "blank-1", ... are used
-    // for all the created nodes.
-    println!(
-        "Created person named 'Alice' with uid = {}",
-        assigned.uids["blank-0"]
-    );
+    println!("All created nodes (map from blank node names to uids):\n");
 
-    println!("All created nodes (map from blank node names to uids):");
     for (key, val) in assigned.uids.iter() {
         println!("\t{} => {}", key, val);
     }
-}
 
-fn query_data(dgraph: &Dgraph) {
     let query = r#"query all($a: string){
         me(func: eq(name, $a)) {
+            uid
             name
             age
             married
@@ -140,24 +143,7 @@ fn query_data(dgraph: &Dgraph) {
         .new_readonly_txn()
         .query_with_vars(query, vars)
         .expect("query");
-    let root: Root = serde_json::from_slice(&resp.json).expect("parsing");
-    println!("Root: {:#?}", root);
-}
+    let root: Root = serde_json::from_slice(&resp.json).expect("Failed to convert slice to JSON.");
 
-fn main() {
-    println!("connect to dgraph via grpc at localhost:9080");
-
-    let dgraph = make_dgraph!(dgraph::new_dgraph_client("localhost:19080"));
-
-    println!("dropping all schema");
-    drop_all(&dgraph);
-
-    println!("setup schema");
-    set_schema(&dgraph);
-
-    println!("push data");
-    create_data(&dgraph);
-
-    println!("query");
-    query_data(&dgraph);
+    println!("\nQuery result for `eq(name, Alice)`:\n\n{:#?}", root);
 }
--- examples/tls/main.rs
+++ examples/tls/main.rs
@@ -8,40 +8,42 @@ use dgraph::{make_dgraph, Dgraph};
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct Root {
-    pub me: Vec<Person>,
+struct Root {
+    me: Vec<Person>,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct School {
-    pub name: String,
+struct School {
+    name: String,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct Location {
+struct Location {
     #[serde(rename = "type")]
-    pub kind: String,
-    pub coordinates: Vec<f64>,
+    kind: String,
+    coordinates: Vec<f64>,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct Person {
-    pub name: String,
-    pub age: Option<u8>,
-    pub dob: Option<DateTime<Utc>>,
-    pub married: Option<bool>,
-    pub friend: Option<Vec<Person>>,
-    pub loc: Option<Location>,
-    pub school: Option<Vec<School>>,
+struct Person {
+    name: String,
+    age: Option<u8>,
+    dob: Option<DateTime<Utc>>,
+    married: Option<bool>,
+    friend: Option<Vec<Person>>,
+    loc: Option<Location>,
+    school: Option<Vec<School>>,
 }
 
-fn drop_all(dgraph: &Dgraph) {
-    let op_cleanup = dgraph::Operation {
+fn drop_schema(dgraph: &Dgraph) {
+    let op_drop = dgraph::Operation {
         drop_all: true,
         ..Default::default()
     };
 
-    dgraph.alter(&op_cleanup).expect("drop schema");
+    dgraph.alter(&op_drop).expect("Failed to drop schema.");
+
+    println!("Dropped the schema.");
 }
 
 fn set_schema(dgraph: &Dgraph) {
@@ -57,17 +59,50 @@ fn set_schema(dgraph: &Dgraph) {
         ..Default::default()
     };
 
-    dgraph.alter(&op_schema).expect("set schema");
+    dgraph.alter(&op_schema).expect("Failed to set schema.");
+
+    println!("Altered schema.");
 }
 
-fn create_data(dgraph: &Dgraph) {
+fn open_cert_file(path: &str) -> Vec<u8> {
+    if let Result::Ok(file) = File::open(path) {
+        let mut reader = BufReader::new(file);
+        let mut contents = String::new();
+        reader
+            .read_to_string(&mut contents)
+            .unwrap_or_else(|_| panic!("Read contents of file {} into string.", path));
+
+        contents.into_bytes()
+    } else {
+        panic!("File {} not found!", path);
+    }
+}
+
+fn main() {
+    let root_ca = open_cert_file("./tls/ca.crt");
+    let cert = open_cert_file("./tls/client.user.crt");
+    let private_key = open_cert_file("./tls/client.user.key");
+
+    let dgraph = make_dgraph!(dgraph::new_secure_dgraph_client(
+        "localhost:19080",
+        root_ca,
+        cert,
+        private_key
+    ));
+
+    println!("Connected to dgraph via gRPC at localhost:19080.");
+
+    drop_schema(&dgraph);
+    set_schema(&dgraph);
+
     let mut txn = dgraph.new_txn();
 
-    let dob = Utc.ymd(1980, 1, 1).and_hms(23, 0, 0);
-    // While setting an object if a struct has a Uid then its properties in the graph are updated
-    // else a new node is created.
-    // In the example below new nodes for Alice, Bob and Charlie and school are created (since they
-    // dont have a Uid).
+    // While setting an object if a struct has a Uid then its properties in
+    // the graph are updated. Else a new node is created.
+    //
+    // In the example below new nodes for Alice, Bob and Charlie and school
+    // are created (since they don't have a Uid).
+    let date_of_birth = Utc.ymd(1980, 1, 1).and_hms(23, 0, 0);
     let p = Person {
         name: "Alice".to_string(),
         age: Some(26),
@@ -76,7 +111,7 @@ fn create_data(dgraph: &Dgraph) {
             kind: "Point".to_string(),
             coordinates: vec![1.1f64, 2f64],
         }),
-        dob: Some(dob),
+        dob: Some(date_of_birth),
         friend: Some(vec![
             Person {
                 name: "Bob".to_string(),
@@ -96,30 +131,21 @@ fn create_data(dgraph: &Dgraph) {
 
     // Run mutation
     let mut mutation = dgraph::Mutation::new();
-    mutation.set_set_json(serde_json::to_vec(&p).expect("invalid json"));
-    let assigned = txn.mutate(mutation).expect("failed to create data");
+    mutation.set_set_json(serde_json::to_vec(&p).expect("Failed to serialize JSON."));
+    let assigned = txn.mutate(mutation).expect("Failed to create data.");
 
     // Commit transaction
-    txn.commit().expect("Fail to commit mutation");
+    txn.commit().expect("Failed to commit mutation");
 
-    // Get uid of the outermost object (person named "Alice").
-    // Assigned#getUidsMap() returns a map from blank node names to uids.
-    // For a json mutation, blank node names "blank-0", "blank-1", ... are used
-    // for all the created nodes.
-    println!(
-        "Created person named 'Alice' with uid = {}",
-        assigned.uids["blank-0"]
-    );
+    println!("All created nodes (map from blank node names to uids):\n");
 
-    println!("All created nodes (map from blank node names to uids):");
     for (key, val) in assigned.uids.iter() {
         println!("\t{} => {}", key, val);
     }
-}
 
-fn query_data(dgraph: &Dgraph) {
     let query = r#"query all($a: string){
         me(func: eq(name, $a)) {
+            uid
             name
             age
             married
@@ -143,47 +169,7 @@ fn query_data(dgraph: &Dgraph) {
         .new_readonly_txn()
         .query_with_vars(query, vars)
         .expect("query");
-    let root: Root = serde_json::from_slice(&resp.json).expect("parsing");
-    println!("Root: {:#?}", root);
-}
+    let root: Root = serde_json::from_slice(&resp.json).expect("Failed to convert slice to JSON.");
 
-fn open_cert_file(path: &str) -> Vec<u8> {
-    if let Result::Ok(file) = File::open(path) {
-        let mut reader = BufReader::new(file);
-        let mut contents = String::new();
-        reader
-            .read_to_string(&mut contents)
-            .unwrap_or_else(|_| panic!("Read contents of file {} into string.", path));
-
-        contents.into_bytes()
-    } else {
-        panic!("File {} not found!", path);
-    }
-}
-
-fn main() {
-    println!("connect to dgraph via grpc at localhost:9080");
-
-    let root_ca = open_cert_file("./tls/ca.crt");
-    let cert = open_cert_file("./tls/client.user.crt");
-    let private_key = open_cert_file("./tls/client.user.key");
-
-    let dgraph = make_dgraph!(dgraph::new_secure_dgraph_client(
-        "localhost:9080",
-        root_ca,
-        cert,
-        private_key
-    ));
-
-    println!("dropping all schema");
-    drop_all(&dgraph);
-
-    println!("setup schema");
-    set_schema(&dgraph);
-
-    println!("push data");
-    create_data(&dgraph);
-
-    println!("query");
-    query_data(&dgraph);
+    println!("\nQuery result for `eq(name, Alice)`:\n\n{:#?}", root);
 }
