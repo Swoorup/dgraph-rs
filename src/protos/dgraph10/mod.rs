1d3375aa95074b7d9032f40850c6bd018a63b2c2
Ondrej Slinták <ondrowan@gmail.com>
2020-07-20 18:48:17 +0200

    Regenerate protobuf code

--- src/protos/dgraph10/mod.rs
+++ src/protos/dgraph10/mod.rs
@@ -1,6 +1,2 @@
-extern crate futures;
-extern crate grpcio;
-extern crate protobuf;
-
 pub mod api;
 pub mod api_grpc;
--- src/protos/dgraph11/api_grpc.rs
+++ src/protos/dgraph11/api_grpc.rs
@@ -1,7 +1,7 @@
 // This file is generated. Do not edit
 // @generated
 
-// https://github.com/rust-lang/rust-clippy/issues/702
+// https://github.com/Manishearth/rust-clippy/issues/702
 #![allow(unknown_lints)]
 #![allow(clippy::all)]
 
@@ -18,40 +18,77 @@
 #![allow(unused_imports)]
 #![allow(unused_results)]
 
-const METHOD_DGRAPH_LOGIN: ::grpcio::Method<super::api::LoginRequest, super::api::Response> = ::grpcio::Method {
-    ty: ::grpcio::MethodType::Unary,
-    name: "/api.Dgraph/Login",
-    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-};
+const METHOD_DGRAPH_LOGIN: ::grpcio::Method<super::api::LoginRequest, super::api::Response> =
+    ::grpcio::Method {
+        ty: ::grpcio::MethodType::Unary,
+        name: "/api.Dgraph/Login",
+        req_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+        resp_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+    };
 
-const METHOD_DGRAPH_QUERY: ::grpcio::Method<super::api::Request, super::api::Response> = ::grpcio::Method {
-    ty: ::grpcio::MethodType::Unary,
-    name: "/api.Dgraph/Query",
-    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-};
+const METHOD_DGRAPH_QUERY: ::grpcio::Method<super::api::Request, super::api::Response> =
+    ::grpcio::Method {
+        ty: ::grpcio::MethodType::Unary,
+        name: "/api.Dgraph/Query",
+        req_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+        resp_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+    };
 
-const METHOD_DGRAPH_ALTER: ::grpcio::Method<super::api::Operation, super::api::Payload> = ::grpcio::Method {
-    ty: ::grpcio::MethodType::Unary,
-    name: "/api.Dgraph/Alter",
-    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-};
+const METHOD_DGRAPH_ALTER: ::grpcio::Method<super::api::Operation, super::api::Payload> =
+    ::grpcio::Method {
+        ty: ::grpcio::MethodType::Unary,
+        name: "/api.Dgraph/Alter",
+        req_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+        resp_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+    };
 
-const METHOD_DGRAPH_COMMIT_OR_ABORT: ::grpcio::Method<super::api::TxnContext, super::api::TxnContext> = ::grpcio::Method {
+const METHOD_DGRAPH_COMMIT_OR_ABORT: ::grpcio::Method<
+    super::api::TxnContext,
+    super::api::TxnContext,
+> = ::grpcio::Method {
     ty: ::grpcio::MethodType::Unary,
     name: "/api.Dgraph/CommitOrAbort",
-    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
+    req_mar: ::grpcio::Marshaller {
+        ser: ::grpcio::pb_ser,
+        de: ::grpcio::pb_de,
+    },
+    resp_mar: ::grpcio::Marshaller {
+        ser: ::grpcio::pb_ser,
+        de: ::grpcio::pb_de,
+    },
 };
 
-const METHOD_DGRAPH_CHECK_VERSION: ::grpcio::Method<super::api::Check, super::api::Version> = ::grpcio::Method {
-    ty: ::grpcio::MethodType::Unary,
-    name: "/api.Dgraph/CheckVersion",
-    req_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-    resp_mar: ::grpcio::Marshaller { ser: ::grpcio::pb_ser, de: ::grpcio::pb_de },
-};
+const METHOD_DGRAPH_CHECK_VERSION: ::grpcio::Method<super::api::Check, super::api::Version> =
+    ::grpcio::Method {
+        ty: ::grpcio::MethodType::Unary,
+        name: "/api.Dgraph/CheckVersion",
+        req_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+        resp_mar: ::grpcio::Marshaller {
+            ser: ::grpcio::pb_ser,
+            de: ::grpcio::pb_de,
+        },
+    };
 
 #[derive(Clone)]
 pub struct DgraphClient {
@@ -65,7 +102,11 @@ impl DgraphClient {
         }
     }
 
-    pub fn login_opt(&self, req: &super::api::LoginRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::api::Response> {
+    pub fn login_opt(
+        &self,
+        req: &super::api::LoginRequest,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<super::api::Response> {
         self.client.unary_call(&METHOD_DGRAPH_LOGIN, req, opt)
     }
 
@@ -73,15 +114,26 @@ impl DgraphClient {
         self.login_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn login_async_opt(&self, req: &super::api::LoginRequest, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
+    pub fn login_async_opt(
+        &self,
+        req: &super::api::LoginRequest,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
         self.client.unary_call_async(&METHOD_DGRAPH_LOGIN, req, opt)
     }
 
-    pub fn login_async(&self, req: &super::api::LoginRequest) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
+    pub fn login_async(
+        &self,
+        req: &super::api::LoginRequest,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
         self.login_async_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn query_opt(&self, req: &super::api::Request, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::api::Response> {
+    pub fn query_opt(
+        &self,
+        req: &super::api::Request,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<super::api::Response> {
         self.client.unary_call(&METHOD_DGRAPH_QUERY, req, opt)
     }
 
@@ -89,15 +141,26 @@ impl DgraphClient {
         self.query_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn query_async_opt(&self, req: &super::api::Request, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
+    pub fn query_async_opt(
+        &self,
+        req: &super::api::Request,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
         self.client.unary_call_async(&METHOD_DGRAPH_QUERY, req, opt)
     }
 
-    pub fn query_async(&self, req: &super::api::Request) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
+    pub fn query_async(
+        &self,
+        req: &super::api::Request,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Response>> {
         self.query_async_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn alter_opt(&self, req: &super::api::Operation, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::api::Payload> {
+    pub fn alter_opt(
+        &self,
+        req: &super::api::Operation,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<super::api::Payload> {
         self.client.unary_call(&METHOD_DGRAPH_ALTER, req, opt)
     }
 
@@ -105,56 +168,120 @@ impl DgraphClient {
         self.alter_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn alter_async_opt(&self, req: &super::api::Operation, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Payload>> {
+    pub fn alter_async_opt(
+        &self,
+        req: &super::api::Operation,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Payload>> {
         self.client.unary_call_async(&METHOD_DGRAPH_ALTER, req, opt)
     }
 
-    pub fn alter_async(&self, req: &super::api::Operation) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Payload>> {
+    pub fn alter_async(
+        &self,
+        req: &super::api::Operation,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Payload>> {
         self.alter_async_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn commit_or_abort_opt(&self, req: &super::api::TxnContext, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::api::TxnContext> {
-        self.client.unary_call(&METHOD_DGRAPH_COMMIT_OR_ABORT, req, opt)
+    pub fn commit_or_abort_opt(
+        &self,
+        req: &super::api::TxnContext,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<super::api::TxnContext> {
+        self.client
+            .unary_call(&METHOD_DGRAPH_COMMIT_OR_ABORT, req, opt)
     }
 
-    pub fn commit_or_abort(&self, req: &super::api::TxnContext) -> ::grpcio::Result<super::api::TxnContext> {
+    pub fn commit_or_abort(
+        &self,
+        req: &super::api::TxnContext,
+    ) -> ::grpcio::Result<super::api::TxnContext> {
         self.commit_or_abort_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn commit_or_abort_async_opt(&self, req: &super::api::TxnContext, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::TxnContext>> {
-        self.client.unary_call_async(&METHOD_DGRAPH_COMMIT_OR_ABORT, req, opt)
+    pub fn commit_or_abort_async_opt(
+        &self,
+        req: &super::api::TxnContext,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::TxnContext>> {
+        self.client
+            .unary_call_async(&METHOD_DGRAPH_COMMIT_OR_ABORT, req, opt)
     }
 
-    pub fn commit_or_abort_async(&self, req: &super::api::TxnContext) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::TxnContext>> {
+    pub fn commit_or_abort_async(
+        &self,
+        req: &super::api::TxnContext,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::TxnContext>> {
         self.commit_or_abort_async_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn check_version_opt(&self, req: &super::api::Check, opt: ::grpcio::CallOption) -> ::grpcio::Result<super::api::Version> {
-        self.client.unary_call(&METHOD_DGRAPH_CHECK_VERSION, req, opt)
+    pub fn check_version_opt(
+        &self,
+        req: &super::api::Check,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<super::api::Version> {
+        self.client
+            .unary_call(&METHOD_DGRAPH_CHECK_VERSION, req, opt)
     }
 
     pub fn check_version(&self, req: &super::api::Check) -> ::grpcio::Result<super::api::Version> {
         self.check_version_opt(req, ::grpcio::CallOption::default())
     }
 
-    pub fn check_version_async_opt(&self, req: &super::api::Check, opt: ::grpcio::CallOption) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Version>> {
-        self.client.unary_call_async(&METHOD_DGRAPH_CHECK_VERSION, req, opt)
+    pub fn check_version_async_opt(
+        &self,
+        req: &super::api::Check,
+        opt: ::grpcio::CallOption,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Version>> {
+        self.client
+            .unary_call_async(&METHOD_DGRAPH_CHECK_VERSION, req, opt)
     }
 
-    pub fn check_version_async(&self, req: &super::api::Check) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Version>> {
+    pub fn check_version_async(
+        &self,
+        req: &super::api::Check,
+    ) -> ::grpcio::Result<::grpcio::ClientUnaryReceiver<super::api::Version>> {
         self.check_version_async_opt(req, ::grpcio::CallOption::default())
     }
-    pub fn spawn<F>(&self, f: F) where F: ::futures::Future<Item = (), Error = ()> + Send + 'static {
+    pub fn spawn<F>(&self, f: F)
+    where
+        F: ::futures::Future<Output = ()> + Send + 'static,
+    {
         self.client.spawn(f)
     }
 }
 
 pub trait Dgraph {
-    fn login(&mut self, ctx: ::grpcio::RpcContext, req: super::api::LoginRequest, sink: ::grpcio::UnarySink<super::api::Response>);
-    fn query(&mut self, ctx: ::grpcio::RpcContext, req: super::api::Request, sink: ::grpcio::UnarySink<super::api::Response>);
-    fn alter(&mut self, ctx: ::grpcio::RpcContext, req: super::api::Operation, sink: ::grpcio::UnarySink<super::api::Payload>);
-    fn commit_or_abort(&mut self, ctx: ::grpcio::RpcContext, req: super::api::TxnContext, sink: ::grpcio::UnarySink<super::api::TxnContext>);
-    fn check_version(&mut self, ctx: ::grpcio::RpcContext, req: super::api::Check, sink: ::grpcio::UnarySink<super::api::Version>);
+    fn login(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        req: super::api::LoginRequest,
+        sink: ::grpcio::UnarySink<super::api::Response>,
+    );
+    fn query(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        req: super::api::Request,
+        sink: ::grpcio::UnarySink<super::api::Response>,
+    );
+    fn alter(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        req: super::api::Operation,
+        sink: ::grpcio::UnarySink<super::api::Payload>,
+    );
+    fn commit_or_abort(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        req: super::api::TxnContext,
+        sink: ::grpcio::UnarySink<super::api::TxnContext>,
+    );
+    fn check_version(
+        &mut self,
+        ctx: ::grpcio::RpcContext,
+        req: super::api::Check,
+        sink: ::grpcio::UnarySink<super::api::Version>,
+    );
 }
 
 pub fn create_dgraph<S: Dgraph + Send + Clone + 'static>(s: S) -> ::grpcio::Service {
